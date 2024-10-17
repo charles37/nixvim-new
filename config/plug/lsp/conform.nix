b4fda2fe380bfdce484e328940ad0f9e762015ec
charles37 <benprevor@gmail.com>
2024-10-17 18:11:54 -0400

    conform upadate

--- config/plug/lsp/conform.nix
+++ config/plug/lsp/conform.nix
@@ -23,4 +23,44 @@
       };
     };
   };
+
+  extraConfigLua = ''
+    -- Initialize a variable to track the state
+    vim.g.format_on_save_enabled = true
+
+    -- Function to toggle format on save
+    local function toggle_format_on_save()
+      vim.g.format_on_save_enabled = not vim.g.format_on_save_enabled
+      if vim.g.format_on_save_enabled then
+        vim.notify("Format on save enabled")
+      else
+        vim.notify("Format on save disabled")
+      end
+    end
+
+    -- Create a user command to toggle format on save
+    vim.api.nvim_create_user_command("ToggleFormatOnSave", toggle_format_on_save, {})
+
+    -- Override Conform's format_on_save function
+    local conform = require("conform")
+    local original_format_on_save = conform.format_on_save
+
+    conform.format_on_save = function(bufnr)
+      if vim.g.format_on_save_enabled then
+        original_format_on_save(bufnr)
+      end
+    end
+  '';
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>tf";
+      action = "<cmd>ToggleFormatOnSave<CR>";
+      options = {
+        silent = true;
+        desc = "Toggle format on save";
+      };
+    }
+  ];
 }
